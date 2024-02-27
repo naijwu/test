87033133afffb3b657b6d31b493c47eb223386c6
naijwu <jaewuchun@gmail.com>
2024-02-26 19:56:45 -0800

    added css to component

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const typescript = require("@rollup/plugin-typescript");
 const dts = require("rollup-plugin-dts");
+const postcss = require("rollup-plugin-postcss");
 
 const packageJson = require("./package.json");
 
@@ -24,12 +25,13 @@ export default [
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
-            // postcss(),
+            postcss(),
         ]
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts.default()]
+        plugins: [dts.default()],
+        external: [/\.css$/],
     }
 ]
